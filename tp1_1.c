7e836b9919455199bc65591658c4d7cba03d15a7
Coronel Exequiel <execoronel14f@gmail.com>
2022-03-18 18:15:17 -0300

    repaso de punteros realizado

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 
 int main () {
-    printf("Hola mundo");
+    int *p_punt, numero=10;
+    p_punt=&numero;
+    printf("Hola mundo\n");
+    printf("contenido de la variable a la que apunta el puntero: %d\n",*p_punt);
+    printf("direcci%cn de memoria almacenada por el puntero: %p\n",162,p_punt);
+    printf("direcci%cn de memoria de la variable: %p\n",162,&numero);
+    printf("direcci%cn de memoria del puntero: %p\n",162,&p_punt);
+    printf("tama%co de memoria usado por la variable numero: %d Bytes",164,sizeof(numero));
+
     return 0;
 }
